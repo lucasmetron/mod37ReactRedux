93a30a5da265b37c5d5c4adcac92260ae5fcca96
lucasmetron <lucasmetron@gmail.com>
2021-07-07 16:39:26 -0300

    redux no react

---
+++ src/actions/counterActions.js
@@ -0,0 +1,5 @@
+
+export const incremente = (value) => { return { type: 'INCREMENTE', payload: value || 1 } }
+export const decremente = (value) => { return { type: 'DECREMENTE', payload: value || 1 } }
+
+
